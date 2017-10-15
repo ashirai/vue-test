01904efc5b5d287bc3bce4b64d20a4d56990a801
ashirai <akira.shirai@gmail.com>
2017-10-14 18:21:34 -0600

    added full screen overlay.

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,9 +1,12 @@
 <template>
-  <video poster="../assets/home/concert.jpg" id="bgvid" playsinline autoplay muted loop>
-    <!-- WCAG general accessibility recommendation is that media such as background video play through only once. Loop turned on for the purposes of illustration; if removed, the end of the video will fade in the same way created by pressing the "Pause" button  -->
-    <source src="../assets/home/concert.webm" type="video/webm">
-    <source src="../assets/home/concert.mp4" type="video/mp4">
-  </video>
+  <div class="home">
+    <video poster="../assets/home/concert.jpg" id="bgvid" playsinline autoplay muted loop>
+      <!-- WCAG general accessibility recommendation is that media such as background video play through only once. Loop turned on for the purposes of illustration; if removed, the end of the video will fade in the same way created by pressing the "Pause" button  -->
+      <source src="../assets/home/concert.webm" type="video/webm">
+      <source src="../assets/home/concert.mp4" type="video/mp4">
+    </video>
+    <div class="full-screen-overlay"></div>
+  </div>
 </template>
 
 <script>
@@ -18,19 +21,37 @@
 </script>
 
 <style lang="scss" scoped>
-  video {
-    position: fixed;
-    top: 50%;
-    left: 50%;
-    min-width: 100%;
-    min-height: 100%;
-    width: auto;
-    height: auto;
-    z-index: -100;
-    transform: translateX(-50%) translateY(-50%);
-    background: url('../assets/home/concert.jpg') no-repeat;
-    background-size: cover;
-    transition: 1s opacity;
+  .home{
+    video {
+      position: fixed;
+      top: 50%;
+      left: 50%;
+      min-width: 100%;
+      min-height: 100%;
+      width: auto;
+      height: auto;
+      z-index: -100;
+      transform: translateX(-50%) translateY(-50%);
+      background: url('../assets/home/concert.jpg') no-repeat;
+      background-size: cover;
+      transition: 1s opacity;
+    }
+
+    .full-screen-overlay{
+      position: fixed;
+      top: 50%;
+      left: 50%;
+      min-width: 100%;
+      min-height: 100%;
+      width: auto;
+      height: auto;
+      z-index: -99;
+      transform: translateX(-50%) translateY(-50%);
+      background: rgba(0,0,0,0.7);
+      background-size: cover;
+      transition: 1s opacity;
+    }
+
   }
 
 </style>

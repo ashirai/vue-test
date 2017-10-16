843bc460dadd191e7a09bed416ba6c61ff9213e0
ashirai <akira.shirai@gmail.com>
2017-10-16 12:46:47 -0600

    added font-awesome plugin and enabled it

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import App from './App'
 import router from './router'
 import iView from 'iview'
 import 'iview/dist/styles/iview.css'
+import 'font-awesome/css/font-awesome.css'
 
 Vue.config.productionTip = false
 Vue.use(iView)
